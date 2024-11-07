d0d5caef2e893912ef87ca6c2193dfab54b1e599
Sneha Rathoe <rathoresneha22@gmail.com>
2024-11-07 15:07:24 +0530

    All Files

--- src/test/java/com/thetestingacademy/ex_1410/CURD/TestCaseIntegration.java
+++ src/test/java/com/thetestingacademy/ex_1410/CURD/TestCaseIntegration.java
@@ -81,6 +81,36 @@ public class TestCaseIntegration {
 
     @Test
     public void test_update_request_put() {
+        token = getToken();
+        bookingId= getBookingID();
+
+        String payloadPUT =        "{\n" +
+                "    \"firstname\" : \"shuchi\",\n" +
+                "    \"lastname\" : \"rathore\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2014-01-01\",\n" +
+                "        \"checkout\" : \"2014-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"dinner\"\n" +
+                "}";
+
+
+
+
+        String token = "0d5c10c0a1064ce";
+        String bookingid ="6118";
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+        requestSpecification.basePath( "/booking/"+bookingId);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.cookies("token",token);
+        requestSpecification.body(payloadPUT).log().all();
+
+        Response response = requestSpecification.when().put();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
 
 
     }
